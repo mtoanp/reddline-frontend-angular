74ded3eb98625f52a7d9a21d75af7ab5bc0e3d00
mtoan <mto.work@outlook.com>
2023-09-19 13:20:34 +0200

    etudiant CRUD

--- src/app/etudiant/show-etudiant/show-etudiant.component.ts
+++ src/app/etudiant/show-etudiant/show-etudiant.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Etudiant } from 'src/app/model/etudiant.model';
+import { EtudiantService } from 'src/app/service/etudiant.service';
 
 @Component({
   selector: 'app-show-etudiant',
   templateUrl: './show-etudiant.component.html',
   styleUrls: ['./show-etudiant.component.scss']
 })
-export class ShowEtudiantComponent {
+export class ShowEtudiantComponent implements OnInit {
 
+  etudiant!:Etudiant;
+  id!:number;
 
+  constructor( private service:EtudiantService,
+               private activateRoute:ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.activateRoute.snapshot.params['id'];
+    this.service.getById(this.id).subscribe({
+      next: (etudiant) => {
+        this.etudiant = etudiant;
+      }, 
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 
 }
